55ebcfcbea3320e68517dfd2e48239c68aec6c0d
carloskes <cscorrea@utpl.edu.ec>
2025-01-09 12:24:06 -0500

    actualizado

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,47 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int n1, n2, n3, n4;
+        System.out.println("ingrese la nota 1");
+        n1 = entrada.nextInt();
+        System.out.println("ingrese la nota 2");
+        n2 = entrada.nextInt();
+        System.out.println("ingrese la nota 3");
+        n3 = entrada.nextInt();
+        System.out.println("ingrese la nota 4");
+        n4 = entrada.nextInt();
+
+        String tiponota = cualitativo(n1, n2, n3, n4);
+        String salida = "el promedio de las notas: " + n1 + ", " + n2 + ", " + n3 + ", " + n4 + " es: " + tiponota;
+        salida = String.format("%s", mensaje(salida));
+        System.out.println(salida);
     }
-    
+
+    public static String cualitativo(int a, int b, int c, int d) {
+        int promedio = (a + b + c + d) / 4;
+        String tipo = "";
+        if (promedio >= 0 && promedio <= 5) {
+            tipo = "Regular";
+        } else {
+            if (promedio > 5.1 && promedio <= 8) {
+                tipo = "Bueno";
+            } else {
+                if (promedio > 8.1 && promedio <= 9) {
+                    tipo = "Muy bueno";
+                } else {
+                    if (promedio > 9.1 && promedio <= 10) {
+                        tipo = "Sobresaliente";
+                    }
+
+                }
+            }
+        }
+        return tipo;
+    }
+
+    public static String mensaje(String a) {
+        String mayuscula = a.toUpperCase();
+        return mayuscula;
+    }
 }
