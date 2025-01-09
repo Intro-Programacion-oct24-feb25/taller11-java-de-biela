a001324899e0f35aed47fff4ddb9ef79220f8af1
carloskes <cscorrea@utpl.edu.ec>
2025-01-09 11:29:25 -0500

    actualizado

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -17,10 +17,18 @@ public class Problema1 {
     public static void main(String[] args) {
         int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
         imprimirDatos(informacion);
+
     }
 
     public static void imprimirDatos(int[][] datos) {
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                if (datos[i][j] % 2 == 0) {
+                    System.out.println(datos[i][j]);
+                }
+
+            }
+        }
 
     }
-
 }
