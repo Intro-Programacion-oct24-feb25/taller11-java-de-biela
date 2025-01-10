f6f7bcaee53ae3e9cff16901422fc11c7211287b
carloskes <cscorrea@utpl.edu.ec>
2025-01-10 09:13:08 -0500

    actualizado

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,48 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+    public static int[][] sumarMatrices(int[][] matriz1, int[][] matriz2) {
+        // Obtener dimensiones de las matrices
+        int filas = matriz1.length;
+        int columnas = matriz1[0].length;
+
+        // Crear una nueva matriz para almacenar el resultado
+        int[][] resultado = new int[filas][columnas];
+
+        // Realizar la suma elemento por elemento
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        
+        int[][] informacion1 = {
+            {1, 2, 3},
+            {10, 20, 30},
+            {100, 200, 300}
+        };
+
+        int[][] informacion2 = {
+            {1, 2, 3},
+            {10, 20, 30},
+            {100, 200, 300}
+        };
+
+        
+        int[][] suma = sumarMatrices(informacion1, informacion2);
+
+        
+        System.out.println("Resultado de los dos arreglos:");
+        for (int i = 0; i < suma.length; i++) {
+            for (int j = 0; j < suma[i].length; j++) {
+                System.out.print(suma[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
