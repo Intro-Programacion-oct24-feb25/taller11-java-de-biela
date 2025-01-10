a183344413821ac1f6a6ad7db9cd9d413ff0fd64
carloskes <cscorrea@utpl.edu.ec>
2025-01-10 09:28:55 -0500

    actualizado

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package problemas;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
 public class Problema7 {
+
+    public static void main(String[] args) {
+        String[] ciudades = generarArreglo();
+        imprimirValores(ciudades);
+    }
+
+    public static String[] generarArreglo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de elementos del arreglo: ");
+        int numElementos = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] arreglo = new String[numElementos];
+        for (int i = 0; i < numElementos; i++) {
+            System.out.print("Ingrese una ciudad de Ecuador: ");
+            arreglo[i] = scanner.nextLine();
+        }
+        return arreglo;
+    
+}
+    public static void imprimirValores(String[] arreglo) {
+        System.out.println("Ciudades con 4 o 5 caracteres:");
+        for (String ciudad : arreglo) {
+            if (ciudad.length() == 4 || ciudad.length() == 5) {
+                System.out.println(ciudad);
+            }
+        }
+    }
+
     
 }
